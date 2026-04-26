f4bcc702ba237b83cc813fd3d2bc33c8df3ce22c
Alex LaFroscia <alex@lafroscia.com>
2026-04-26 14:06:11 -0400

    add `Search` component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,7 @@ export { default as ExtraButton } from './components/ExtraButton.svelte';
 export { default as Modal } from './components/Modal.svelte';
 export { default as MomentFormat } from './components/MomentFormat.svelte';
 export { default as ProgressBar } from './components/ProgressBar.svelte';
+export { default as Search } from './components/Search.svelte';
 
 export { setTooltipAttachment as setTooltip } from './attachments/set-tooltip.svelte';
 

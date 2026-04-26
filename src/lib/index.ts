4b6c832cff137adb93f0190d70a4ee136872d76e
Alex LaFroscia <alex@lafroscia.com>
2026-04-26 14:01:28 -0400

    add `ExtraButton` component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from './components/Button.svelte';
 export { default as Color } from './components/Color.svelte';
 export { default as Dropdown } from './components/Dropdown.svelte';
+export { default as ExtraButton } from './components/ExtraButton.svelte';
 export { default as Modal } from './components/Modal.svelte';
 
 export { setTooltipAttachment as setTooltip } from './attachments/set-tooltip.svelte';

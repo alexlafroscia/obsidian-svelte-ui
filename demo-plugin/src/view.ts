a6038541b6169ebe6cfe4c644e43e37aae750e55
Alex LaFroscia <alex@lafroscia.com>
2026-04-29 23:54:27 -0400

    add svelte rendering to `demo-plugin`

--- demo-plugin/src/view.ts
+++ demo-plugin/src/view.ts
@@ -1,4 +1,7 @@
 import { ItemView, WorkspaceLeaf } from 'obsidian';
+import { SvelteComponent } from 'obsidian-svelte-ui';
+
+import ComponentDemo from './ComponentDemo.svelte';
 
 export const VIEW_TYPE = 'demo-obsidian-svelte-ui-components';
 
@@ -16,8 +19,11 @@ export class DemoObsidianSvelteUIComponentsView extends ItemView {
 	}
 
 	async onOpen(): Promise<void> {
-		this.contentEl.empty();
+		this.addChild(
+			new SvelteComponent(ComponentDemo, {
+				target: this.contentEl,
+				props: {}
+			})
+		);
 	}
-
-	async onClose(): Promise<void> {}
 }
---
+++ demo-plugin/svelte-config.mjs
@@ -0,0 +1,7 @@
+import { sveltePreprocess } from 'svelte-preprocess';
+
+export default {
+	compilerOptions: { css: 'injected' },
+	preprocess: sveltePreprocess()
+};
+e;

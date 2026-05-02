6bdd0fabceca452bea4216af187dfad1cff2bc7b
Alex LaFroscia <alex@lafroscia.com>
2026-05-02 09:19:11 -0400

    add `dev` script

--- demo-plugin/src/ComponentDemo.svelte
+++ demo-plugin/src/ComponentDemo.svelte
@@ -1,1 +1,24 @@
-<p>hello world</p>
+<script lang="ts">
+	import { Tabs, Tab } from 'obsidian-svelte-ui';
+
+	import ComponentShowcase from './ComponentShowcase.svelte';
+
+	import TextDemo from './demos/TextDemo.svelte';
+	import TextDemoSource from './demos/TextDemo.svelte?raw';
+	import TextAreaDemo from './demos/TextAreaDemo.svelte';
+	import TextAreaDemoSource from './demos/TextAreaDemo.svelte?raw';
+</script>
+
+<Tabs>
+	<Tab title="Text">
+		<ComponentShowcase source={TextDemoSource}>
+			<TextDemo />
+		</ComponentShowcase>
+	</Tab>
+
+	<Tab title="TextArea">
+		<ComponentShowcase source={TextAreaDemoSource}>
+			<TextAreaDemo />
+		</ComponentShowcase>
+	</Tab>
+</Tabs>
---
+++ demo-plugin/src/demos/TextAreaDemo.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { TextArea } from 'obsidian-svelte-ui';
+
+	let textValue = $state('Initial Value');
+</script>
+
+<TextArea
+	value={textValue}
+	placeholder="Enter a value"
+	onChange={(value) => {
+		textValue = value;
+	}}
+/>
+
+<p>Value: {textValue}</p>
